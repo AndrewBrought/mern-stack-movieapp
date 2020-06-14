0b7a1bcdde0932c9b85c383b7cb9f22c626371e1
Andrew Brought <andrewbrought@gmail.com>
2020-06-13 19:31:19 -0500

    building MERN stack app

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = '204b56a00c1ac9ceacc1e6f68f7640ea';
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
